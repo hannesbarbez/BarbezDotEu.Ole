3c841562c637fb01610c17e662a50691df2f94c0
Hannes Barbez <hannesbarbez@hotmail.com>
2025-05-22 22:43:33 +0200

    Initial code

---
+++ BarbezDotEu.Ole/OLECMD.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Runtime.InteropServices;
+
+namespace BarbezDotEu.Ole
+{
+    /// <summary>
+    /// OLE command structure.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public class OLECMD
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        [MarshalAs(UnmanagedType.U4)]
+        public int cmdID;
+        /// <summary>
+        /// Flags associated with cmdID.
+        /// </summary>
+        [MarshalAs(UnmanagedType.U4)]
+        public int cmdf;
+    }
+}
---
+++ BarbezDotEu.Ole/OLECMDID.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the MIT License.
+
+namespace BarbezDotEu.Ole
+{
+    /// <summary>
+    /// Specifies OLE command IDs from Microsoft.VisualStudio.OLE.Interop (Visual Studio 2010 SDK).
+    /// </summary>
+    public enum OLECMDID
+    {
+        /// <summary>Open command.</summary>
+        OLECMDID_OPEN = 1,
+        /// <summary>New command.</summary>
+        OLECMDID_NEW,
+        /// <summary>Save command.</summary>
+        OLECMDID_SAVE,
+        /// <summary>Save As command.</summary>
+        OLECMDID_SAVEAS,
+        /// <summary>Save Copy As command.</summary>
+        OLECMDID_SAVECOPYAS,
+        /// <summary>Print command.</summary>
+        OLECMDID_PRINT,
+        /// <summary>Print Preview command.</summary>
+        OLECMDID_PRINTPREVIEW,
+        /// <summary>Page Setup command.</summary>
+        OLECMDID_PAGESETUP,
+        /// <summary>Spell command.</summary>
+        OLECMDID_SPELL,
+        /// <summary>Properties command.</summary>
+        OLECMDID_PROPERTIES,
+        /// <summary>Cut command.</summary>
+        OLECMDID_CUT,
+        /// <summary>Copy command.</summary>
+        OLECMDID_COPY,
+        /// <summary>Paste command.</summary>
+        OLECMDID_PASTE,
+        /// <summary>Paste Special command.</summary>
+        OLECMDID_PASTESPECIAL,
+        /// <summary>Undo command.</summary>
+        OLECMDID_UNDO,
+        /// <summary>Redo command.</summary>
+        OLECMDID_REDO,
+        /// <summary>Select All command.</summary>
+        OLECMDID_SELECTALL,
+        /// <summary>Clear Selection command.</summary>
+        OLECMDID_CLEARSELECTION,
+        /// <summary>Zoom command.</summary>
+        OLECMDID_ZOOM,
+        /// <summary>Get Zoom Range command.</summary>
+        OLECMDID_GETZOOMRANGE,
+        /// <summary>Update Commands command.</summary>
+        OLECMDID_UPDATECOMMANDS,
+        /// <summary>Refresh command.</summary>
+        OLECMDID_REFRESH,
+        /// <summary>Stop command.</summary>
+        OLECMDID_STOP,
+        /// <summary>Hide Toolbars command.</summary>
+        OLECMDID_HIDETOOLBARS,
+        /// <summary>Set Progress Max command.</summary>
+        OLECMDID_SETPROGRESSMAX,
+        /// <summary>Set Progress Position command.</summary>
+        OLECMDID_SETPROGRESSPOS,
+        /// <summary>Set Progress Text command.</summary>
+        OLECMDID_SETPROGRESSTEXT,
+        /// <summary>Set Title command.</summary>
+        OLECMDID_SETTITLE,
+        /// <summary>Set Download State command.</summary>
+        OLECMDID_SETDOWNLOADSTATE,
+        /// <summary>Stop Download command.</summary>
+        OLECMDID_STOPDOWNLOAD,
+        /// <summary>On Toolbar Activated command.</summary>
+        OLECMDID_ONTOOLBARACTIVATED,
+        /// <summary>Find command.</summary>
+        OLECMDID_FIND,
+        /// <summary>Delete command.</summary>
+        OLECMDID_DELETE,
+        /// <summary>HTTP Equiv command.</summary>
+        OLECMDID_HTTPEQUIV,
+        /// <summary>HTTP Equiv Done command.</summary>
+        OLECMDID_HTTPEQUIV_DONE,
+        /// <summary>Enable Interaction command.</summary>
+        OLECMDID_ENABLE_INTERACTION,
+        /// <summary>On Unload command.</summary>
+        OLECMDID_ONUNLOAD,
+        /// <summary>PropertyBag2 command.</summary>
+        OLECMDID_PROPERTYBAG2,
+        /// <summary>Pre Refresh command.</summary>
+        OLECMDID_PREREFRESH,
+        /// <summary>Show Script Error command.</summary>
+        OLECMDID_SHOWSCRIPTERROR,
+        /// <summary>Show Message command.</summary>
+        OLECMDID_SHOWMESSAGE,
+        /// <summary>Show Find command.</summary>
+        OLECMDID_SHOWFIND,
+        /// <summary>Show Page Setup command.</summary>
+        OLECMDID_SHOWPAGESETUP,
+        /// <summary>Show Print command.</summary>
+        OLECMDID_SHOWPRINT,
+        /// <summary>Close command.</summary>
+        OLECMDID_CLOSE,
+        /// <summary>Allow UI-less Save As command.</summary>
+        OLECMDID_ALLOWUILESSSAVEAS,
+        /// <summary>Don't Download CSS command.</summary>
+        OLECMDID_DONTDOWNLOADCSS,
+        /// <summary>Update Page Status command.</summary>
+        OLECMDID_UPDATEPAGESTATUS,
+        /// <summary>Print2 command.</summary>
+        OLECMDID_PRINT2,
+        /// <summary>Print Preview2 command.</summary>
+        OLECMDID_PRINTPREVIEW2,
+        /// <summary>Set Print Template command.</summary>
+        OLECMDID_SETPRINTTEMPLATE,
+        /// <summary>Get Print Template command.</summary>
+        OLECMDID_GETPRINTTEMPLATE
+    }
+}
